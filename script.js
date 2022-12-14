63228ad0192eb2f22c3d573fcc87fdaac4ec9f67
Marco Guaspari Worms <marcogworms@gmail.com>
2022-12-13 23:03:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 const github = require('@actions/github');
 
-// Create a new instance of the GitHub client
-const github = new GitHub(process.env.TOKEN);
 const octokit = github.getOctokit(process.env.TOKEN)
 const context = github.context
 
-// Get the pull request information from the context object
 const pullRequest = context.payload.pull_request;
 
-// Create a comment on the pull request
 octokit.issues.createComment({
   owner: pullRequest.user.login,
   repo: pullRequest.head.repo.name,
