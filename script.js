5f29c8bade7e453c10e5c92698411e4e9ba13a6a
Marco Guaspari Worms <marcogworms@gmail.com>
2022-12-15 12:51:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const context = github.context
 
 const pullRequest = context.payload.pull_request;
 
-console.log(JSON.stringify(pullRequest, null, 2))
+// pullRequest.diff_url
+console.log(JSON.stringify(github.context.payload, undefined, 2))
 
 octokit.rest.issues.createComment({
   owner: pullRequest.user.login,
