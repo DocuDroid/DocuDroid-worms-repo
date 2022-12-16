8a748144783eab3a3d0af1133f786e714bffb061
Marco Guaspari Worms <marcogworms@gmail.com>
2022-12-16 00:34:49 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
+const { Configuration, OpenAIApi } = require('openai')
 const github = require('@actions/github')
-const openai = require('openai')
 const axios = require('axios')
 const diff = require('diff')
 
-const octokit = github.getOctokit(process.env.TOKEN)
+const octokit = github.getOctokit(process.env.GH_TOKEN)
 const context = github.context
 
-const pullRequest = context.payload.pull_request;
+const configureOpenai = apiKey => new OpenAIApi(
+  new Configuration({ apiKey: process.env.OPENAI_TOKEN })
+)
+
+const openai = configureOpenai(apiKey)
 
 async function start () {
   
@@ -19,6 +23,17 @@ async function start () {
         .reduce((acc, line) => acc + '\n' + line, '')
     ).join('\n\n')
   ).join('\n\n')
+  
+  const rawResponse = await openai.createEdit({
+    model: "text-davinci-edit-001",
+    input: body,
+    instruction: "fix grammar, don't let a single grammar error pass, this text can't contain grammatical errors",
+    temperature: 0.7,
+    top_p: 1,
+  })
+  const response = rawResponse.data.choices[0].text.trim()
+  
+  const pullRequest = context.payload.pull_request
   
   await octokit.rest.issues.createComment({
     owner: pullRequest.user.login,
