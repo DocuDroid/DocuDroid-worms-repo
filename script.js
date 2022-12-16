df34fd2833b8272cdd95faef7aa114181b1054ce
Marco Guaspari Worms <marcogworms@gmail.com>
2022-12-16 00:11:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const github = require('@actions/github')
 const axios = require('axios')
+const diff = require('diff')
 
 const octokit = github.getOctokit(process.env.TOKEN)
 const context = github.context
@@ -8,7 +9,7 @@ const pullRequest = context.payload.pull_request;
 
 axios.get(pullRequest.diff_url)
   .then(res => {
-    console.log(JSON.stringify(Diff.parsePatch(res.data), null, 2))
+    console.log(JSON.stringify(diff.parsePatch(res.data), null, 2))
   })
 
 octokit.rest.issues.createComment({
