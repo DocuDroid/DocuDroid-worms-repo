5760ca20e9a26bb9d2f5e2d69799820c8269a099
Marco Guaspari Worms <marcogworms@gmail.com>
2022-12-20 11:42:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const openai = new OpenAIApi(
 const pullRequest = context.payload.pull_request
 
 // config instructions for each review type for GPT-Edit
-const prompts = [
+const commands = [
   {
     prompt: `Act as a professional copywriter and coder. Make a pull request review for the following PR diff, there should be no grammars and typos being introduced, answer straightforward and direct suggestions you have for improvements`,
     temperature: 0,
@@ -55,15 +55,15 @@ async function start () {
 //   ).join('\n\n').trim()
   
   // iterates all prompts
-  prompts.forEach(async (prompt, i) => {
+  commands.forEach(async (command, i) => {
     
     // delays 5 seconds between each call so we dont spam apis
     await new Promise(resolve => setTimeout(resolve, i * 10000))
 
     const rawResponse = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: prompt + `\n\n${prDiff.data}\n\n`,
-      temperature: prompt.temperature || 0.5,
+      prompt: command.prompt + `\n\n${prDiff.data}\n\n`,
+      temperature: command.temperature || 0.5,
       top_p: 1,
       max_tokens: 2000,
       frequency_penalty: 0,
@@ -75,7 +75,7 @@ async function start () {
       owner: pullRequest.user.login, // only works for the repo owner atm
       repo: pullRequest.head.repo.name,
       issue_number: pullRequest.number,
-      body: `### DocuDroid Review \n\n${response}`,
+      body: `### DocuDroid Review\n\nInstructions: *${command.prompt}*\nTemperature: *${command.temperature}*\n\n---\n\n${response}`,
     })
 
 }
